6b7985782427c5bb4fe43998e40f091259209fc9
Anna <AB5124@student.jamk.fi>
2021-06-04 12:22:16 +0300

    Added some text and date to Hello.py

---
+++ l05t1/Hello.py
@@ -0,0 +1,9 @@
+# print date and some text
+
+# import the date class
+from datetime import date
+# declare a variable as 'today' using dateclass
+today = date.today()
+# declare  a variable as 'name' and assigne some text to it
+name = 'Anna'
+print("Hello world! Nimeni on", name," ja tänään on ", today, ".")
