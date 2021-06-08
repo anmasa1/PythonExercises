6087ead4877afd268a4e53f6700c03f19a17a5c0
Anna <AB5124@student.jamk.fi>
2021-06-08 08:47:56 +0300

    Added Tehtävä L07T04 to project.

---
+++ exercise07/07ex4.py
@@ -0,0 +1,14 @@
+#Tehtävä L07T04
+
+#Counts the sum, difference, division and multiplication of two integers 
+
+number1 =int(input("Give a number"))
+number2 =int(input("Give a number"))
+sum = number1+number2
+difference = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+print("The sum is",sum)
+print ("The difference is", difference)
+print(" The multiplication is", multiplication)
+print("The division is", division)
