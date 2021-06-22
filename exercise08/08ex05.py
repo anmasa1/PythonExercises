29aface2f22c8f5ce34a260395a229b0a984ce19
Anna <AB5124@student.jamk.fi>
2021-06-22 14:59:14 +0300

    Added exercise Tehtävä L08T05 to project

---
+++ exercise08/08ex05.py
@@ -0,0 +1,34 @@
+#Tehtävä L08T05
+
+number1 = int(input("Give a number"))
+
+if number1 >= 1:
+    result1 = number1
+else: result1 = 0
+
+number2 = int(input("Give a number"))
+
+if number2 >= 1:
+    result2 = number2
+else: result2 = 0
+
+number3 = int(input("Give a number"))
+
+if number3 >= 1:
+    result3 = number3
+else: result3 = 0
+
+number4 = int(input("Give a number"))
+
+if number4 >= 1:
+    result4 = number4
+else: result4 = 0
+
+number5 = int(input("Give a number"))
+
+if number5 >= 1:
+    result5 = number5
+else: result5 = 0
+
+
+print("Result is" ,result1 + result2 + result3 + result4 + result5)
