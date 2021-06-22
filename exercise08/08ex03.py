aaddce8beaa92c0e63a4029f2228f5eef8c29969
Anna <AB5124@student.jamk.fi>
2021-06-22 13:11:42 +0300

    Added exercise Tehtävä L08T03 to project

---
+++ exercise08/08ex03.py
@@ -0,0 +1,10 @@
+#Tehtävä L08T03 
+
+
+number = int(input("Give a number"))
+
+if number == 10 or number == 20:
+    print("Number is 10 or 20")
+elif number == 100 or number == 200:
+    print("Number is 100 or 200")
+else: print(number)
